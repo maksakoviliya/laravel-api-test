4b299ed62f8979d1b9a1b94b16ea938361f46f77
maksakoviliya <maksak_il@mail.ru>
2026-01-31 21:55:54 +0300

    Make fillable fields

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -1,10 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Property extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'price',
+        'bedrooms',
+        'bathrooms',
+        'storeys',
+        'garages',
+    ];
 }
